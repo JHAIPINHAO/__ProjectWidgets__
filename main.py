f78ce7f2279f55222c6e6d22cfa596801699c4ee
Gordon <ABC123@example.com>
2023-04-08 13:22:11 +0800

    modify

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import tkinter as tk
 from parts1 import TopFrame
 from parts2 import MedianFrame
+from parts3 import BottomFrame
 
 class Window(tk.Tk):
     
     def __init__(self):
         super().__init__()
         topFrame = TopFrame(self,borderwidth=0)       
-        topFrame.pack()
+        topFrame.pack()        
         medianFrame = MedianFrame(self,borderwidth=0)
         medianFrame.pack(fill=tk.X)
+        bottomFrame = BottomFrame(self)
+        bottomFrame.pack(fill=tk.X)
 
     def radioButtonEventOfMedianFrame(self,radioButtonValue):
         print(radioButtonValue)
 
-        
-        
+    def listBoxEventOfBottomFrame(self,listBoxValue):
+        print(listBoxValue)
+
+    def comboBoxEventOfBottomFrame(self,comboValue):
+        print(comboValue)
+
 
 def main():
     window = Window()
